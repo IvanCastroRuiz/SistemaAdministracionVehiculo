f0f133b855793bd337dddb9a7edee474235be0d5
IvanCastroRuiz <icastror@hotmail.com>
2022-08-03 23:07:46 -0500

    vista usuario

--- controller/controllerVehiculo.js
+++ controller/controllerVehiculo.js
@@ -4,7 +4,17 @@ export const addVehiculo = (vehiculo) => {
     bd_vehiculo.push(vehiculo);
 }
 
+export const findAll = () => {
+   for (const vehicle of bd_vehiculo) {
+        console.log(vehicle.mostrarDatos());
+    };
+};
+
+export const deleteId = (id) => {
+    
+};
 
 export default {
-    addVehiculo
+    addVehiculo,
+    findAll
 } 
